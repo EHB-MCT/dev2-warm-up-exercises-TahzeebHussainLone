ffc7098a58904cc941cba8faad9927a89e5f3887
TahzeebHussainLone <tahzeeb.lone@student.ehb.be>
2025-02-13 11:31:49 +0100

    1

--- main.js
+++ main.js
@@ -5,9 +5,20 @@ import {
 } from './lib.js';
 
 runTitle();
+warmup1();
 
 function warmup1() {
     console.log("Exercise week 1");
+
+    document.querySelector('#button-1a').addEventListener('click', function () {
+    console.log('Click Ok');
+
+    const p = document.createElement('p');
+    p.innerHTML = "Hello world, I'm Tahzeeb";
+
+    const p = '<p>Hello world, I'm Tahzeeb</p>'
+    document.querySelector('#content-1').innerHTML = p;
+    });
 }
 
 function warmup2() {
